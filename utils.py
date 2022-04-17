618b4f805ad2fd8cea02815e01072252700fa0ee
Александр <shamin_ad@inbox.ru>
2022-04-17 15:23:31 +0300

    corrected remarks

--- utils.py
+++ utils.py
@@ -1,28 +1,36 @@
 import json
 
-__posts = []
 
+def get_posts_all():
 
-def get_posts_all():  # возвращает список всех постов
-    global __posts
-    if type(__posts) not in [list]:
+    """" Возвращает список всех постов """
+
+    posts = []
+    if type(posts) not in [list]:
         raise TypeError('Должен быть список')
     with open('data/data.json', 'r', encoding='utf-8') as file:
-        __posts = json.load(file)
-    return __posts
+        posts = json.load(file)
+    return posts
 
 
-def get_posts_by_user(user_name):  # возвращает посты определенного пользователя
+def get_posts_by_user(user_name):
+
+    """" Возвращает посты определенного пользователя """
+
     post_user = []
     if type(post_user) not in [list]:
         raise TypeError('Должен быть список')
-    for post in __posts:
+    posts = get_posts_all()
+    for post in posts:
         if user_name in post['poster_name']:
             post_user.append(post)
     return post_user
 
 
-def get_comments_by_post_id(post_id):  # возвращает комментарии определенного поста
+def get_comments_by_post_id(post_id):
+
+    """" Возвращает комментарии определенного поста """
+
     with open('data/comments.json', 'r', encoding='utf-8') as file:
         comments = json.load(file)
     post_comments = []
@@ -34,22 +42,30 @@ def get_comments_by_post_id(post_id):  # возвращает комментар
     return post_comments
 
 
-def search_for_posts(query):  # возвращает список постов по ключевому слову
+def search_for_posts(query):
+
+    """" Возвращает список постов по ключевому слову """
+
     posts_query = []
     if type(posts_query) not in [list]:
         raise TypeError('Должен быть список')
-    for post in __posts:
+    posts = get_posts_all()
+    for post in posts:
         if query.lower() in post['content'].lower():
             posts_query.append(post)
     return posts_query
 
 
-def get_post_by_pk(pk):  # возвращает один пост по его идентификатору
-    global post
-    for post in __posts:
-        if pk == post['pk']:
+def get_post_by_pk(pk):
+
+    """" Возвращает один пост по его идентификатору """
+
+    posts = get_posts_all()
+    for post in posts:
+        if type(post) not in [dict]:
+            raise TypeError('Должен быть dict')
+        elif pk == post['pk']:
             return post
-    if type(post) not in [dict]:
-        raise TypeError('Должен быть dict')
+
 
 
